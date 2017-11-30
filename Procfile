7559edad1706fef3a17c8530db264ead0017b128
The Anh <theanh.huy@gmail.com>
2017-11-30 18:04:31 +0700

    Seed: add user admin.

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 production: NODE_ENV=production node src/
-web: NODE_ENV=production node_modules/.bin/sequelize db:migrate && node src/
+web: NODE_ENV=production node_modules/.bin/sequelize db:migrate && node_modules/.bin/sequelize db:seed:all && node src/
---
+++ migrations/seeders/20171130105851-init-user.js
@@ -0,0 +1,23 @@
+'use strict';
+/* eslint-disable no-unused-vars */
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('users', [{
+      email: 'admin1@slotgame.vn',
+      password: '$2a$12$6P3TCCMFZ8hvucjcD67sLOu.4ZOGpWGXcGSd3Rwjc6jlLQA2InJFm',
+      createdAt: '2017-11-30 09:03:55.058+00',
+      updatedAt: '2017-11-30 09:03:55.058+00'
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
