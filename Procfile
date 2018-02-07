b6b873d9f21a3dc0e8082e32cfc3c789f35d12ca
The Anh <theanh.huy@gmail.com>
2018-02-07 16:29:04 +0700

    Clean up migrations.

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-production: NODE_ENV=production node src/
 web: NODE_ENV=production node_modules/.bin/sequelize db:migrate && node src/
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   apps : [
-      {
-        name: "Slotgame",
-        script: "./src",
-        watch: true,
-        env: {
-            "PORT": 3000,
-            "NODE_ENV": "development"
-        },
-        env_production: {
-            "PORT": 80,
-            "NODE_ENV": "production",
-        }
+    {
+      name: "Slotgame",
+      script: "./src",
+      watch: true,
+      env: {
+          "PORT": 3000,
+          "NODE_ENV": "development"
+      },
+      env_production: {
+          "PORT": 80,
+          "NODE_ENV": "production",
       }
+    }
   ]
 }
